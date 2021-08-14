3707916670a351904b2a0e81fe4fa383a202c024
Doğancan Mavideniz <59478623+mavideniz@users.noreply.github.com>
2021-08-14 17:27:47 +0300

    32/100
    More cool animations added.

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,27 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount: CGFloat = 1
-
+    
+    @State private var animationAmount = 0.0
+    
+    
     var body: some View {
-        
-        VStack {
-            Stepper("Scale amount", value: $animationAmount.animation(
-                Animation.easeInOut(duration: 1)
-                    .repeatCount(3, autoreverses: true)
-            ), in: 1...10)
-            Spacer()
-
-            Button("Tap Me") {
-                self.animationAmount += 1
-            }
-            .padding(40)
-            .background(Color.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount)
+        Button("Tap Me") {
+            withAnimation {
+                self.animationAmount += 360
+            }            // do nothing
         }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {

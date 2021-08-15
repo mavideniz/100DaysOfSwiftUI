9f28a6c06b34278a71cb823d3977e472feb0b34c
Doğancan Mavideniz <59478623+mavideniz@users.noreply.github.com>
2021-08-15 20:43:17 +0300

    Day 33/100
    Commit 3, different animation type.

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,29 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    let letters = Array("Hello SwiftUI")
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
-
+    
+    @State private var isShowingRed = false
+    
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count) { num in
-                Text(String(self.letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(self.enabled ? Color.blue : Color.red)
-                    .offset(self.dragAmount)
-                    .animation(Animation.default.delay(Double(num) / 20))
-            }
-        }
-        .gesture(
-            DragGesture()
-                .onChanged { self.dragAmount = $0.translation }
-                .onEnded { _ in
-                    self.dragAmount = .zero
-                    self.enabled.toggle()
+        VStack {
+            Button("Tap Me") {
+                withAnimation{
+                    self.isShowingRed.toggle()
                 }
-        )
+            
+            }
+
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))            }
+            
+        }
     }
 }
 

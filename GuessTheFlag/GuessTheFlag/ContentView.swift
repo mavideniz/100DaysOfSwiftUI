afe70545955bcf3c98bc8caf602a6525a0e29108
Doğancan Mavideniz <59478623+mavideniz@users.noreply.github.com>
2021-08-16 18:13:06 +0300

    34/100
    Animations added for Guess the Flag apps. Animations are stupid!

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -12,9 +12,11 @@ struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var userScore = 0
+ 
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var rotationAmount = 0.0
 
 
     var body: some View {
@@ -39,17 +41,24 @@ struct ContentView: View {
             
         }
         ForEach(0 ..< 3) { number in
+            
             Button(action: {
                 self.flagTapped(number)
+                
                // flag was tapped
-            }) {
-                Image(self.countries[number])
-                    .renderingMode(.original)
-                    .clipShape(Capsule())
-                    .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-                    .shadow(color: .black, radius: 2)
             }
+            ) {
+                    Image(self.countries[number])
+                        .renderingMode(.original)
+                        .clipShape(Capsule())
+                        .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                        .shadow(color: .black, radius: 2)
+                        .opacity(self.showingScore && number != self.correctAnswer ? 0.25 : 1.0)
+                
+            }.rotation3DEffect(.degrees(number == self.correctAnswer ? self.rotationAmount : 0), axis: (x: 0, y: 1, z: 0))
+            
         }
+       
             Text("Your score is: \(userScore)").bold()
                 .font(.largeTitle)
                 .foregroundColor(.white)
@@ -69,6 +78,10 @@ struct ContentView: View {
         if number == correctAnswer {
             scoreTitle = "You are correct!"
             userScore += 1
+            withAnimation(.spring()) {
+                            self.rotationAmount += 360.0
+                        }
+           
         }
         else{
             scoreTitle = "Wrong!, Correct anwser is \(countries[correctAnswer]) "
@@ -79,6 +92,8 @@ struct ContentView: View {
     func askQuestion(){
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+       
+       
     }
     
 }

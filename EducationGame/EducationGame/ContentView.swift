1dac7d0e5342750fd5aa599fdf22dbc59d9e98f7
Doğancan Mavideniz <59478623+mavideniz@users.noreply.github.com>
2021-08-18 17:02:49 +0300

    Last challenge, new features
    I added some of the features that I couldn't do in the project I was working on yesterday. A more feature left to add.

--- EducationGame/EducationGame/ContentView.swift
+++ EducationGame/EducationGame/ContentView.swift
@@ -15,57 +15,118 @@ struct ContentView: View {
     @State private var howManyQuest = 0
     let allMultiplicationTables = Range(1...12)
     let QuestionNumber = [3,5,7]
+    @State private var randomNumber = Int.random(in: 0 ... 10)
+    @State private var answer = 0
+    @State private var answers = [Int]()
+    @State private var userScore = 0
+   
 
     var body: some View {
         
-        
-        if isGameActive {
+        ZStack{
+                Color.yellow
+                    .ignoresSafeArea()
+            if isGameActive {
+                
+                Group{
+                
+                        VStack{
+                            Text("How much is \(multiTable) * \(randomNumber)")
+                            
+                            Text("User Score : \(userScore)")
+                            
+                            ForEach(0..<(answers.count) , id:\.self) { num in
+                                Text("\(answers[num])")
+                                    .onTapGesture {
+                                        if answers[num] == answer {
+                                            userScore += 1
+                                        }
+                                    }
+                            }.onAppear {
+                                answers.shuffle()
+                            }
+                            
+                            Button("Go Back"){
+                                isGameActive.toggle()
+                            }
+                            
+                        }
+                    
+                    }
+                    
+                
+                
+                
+            }else{
+                
+            
             Group{
-                VStack{
-                    Text("test")
+            VStack{
+                //İlk sayfa
+                Text("🧮")
+                    .font(.system(size: 200))
+                    .padding()
+                Text("Multiplication Practise")
+                    .bold()
+                    .font(.system(size: 25))
+                    .padding()
                     
-                    Button("Go Back"){
-                        isGameActive.toggle()
-                    }
-                    
-                }
-            }
-            
-            
-            
-        }else{
-            
-        
-        Group{
-        VStack{
-            
-            //İlk sayfa
-            Text("Hangi çarpım tablosunu kullanmak istersin?")
-            Picker("İstediğin çarpım tablosunu seç", selection: $multiTable){
-                ForEach(allMultiplicationTables, id: \.self) {
-                Text("\($0)")
-                }
-            }
-            .pickerStyle(SegmentedPickerStyle())
-            
-            
-            Text("Kaç soru sorulmasını istersin?")
-                Picker("soru sayısı", selection: $howManyQuest){
-                    ForEach(QuestionNumber, id: \.self){
-                        Text("\($0)")
+                
+                
+                Text("Which number do you want for practise?")
+                Picker("", selection: $multiTable){
+                    ForEach(allMultiplicationTables, id: \.self) {
+                    Text("\($0)")
                     }
                 }
+                .border(Color.red)
+                .padding(20)
+                .onChange(of: multiTable, perform: { val in
+                    randomNumber = Int.random(in: 0 ... 10)
+                    testFunc()
+                })
                 .pickerStyle(SegmentedPickerStyle())
+                
+                
+                Text("How many question do you want?")
+                    Picker("", selection: $howManyQuest){
+                        ForEach(QuestionNumber, id: \.self){
+                            Text("\($0)")
+                        }
+                    }
+                    .frame(width: 200)
+                    .border(Color.red)
+                    .padding(20)
+                    .pickerStyle(SegmentedPickerStyle())
+                
+                Button("Start Game"){
+                    isGameActive.toggle()
+                    createAnswersArray()
+                    //when game start
+                    
+                }
             
-            Button("Start Game"){
-                isGameActive.toggle()
-                
-                //when game start
-                
             }
-        }
+            }
+
         }
     }
+    }
+    
+    func testFunc(){
+        answer = multiTable * randomNumber
+        return
+    }
+    
+    func createAnswersArray() {
+        self.answers.removeAll()
+        let answer1 = multiTable * (randomNumber + 1)
+        let answer2 = multiTable * (randomNumber + 2)
+        let answer3 =  multiTable * (randomNumber - 1)
+        self.answers.append(answer1)
+        self.answers.append(answer2)
+        self.answers.append(answer3)
+        self.answers.append(answer)
     }
 }
 

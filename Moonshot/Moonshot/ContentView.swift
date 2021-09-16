41a9db9b6f462e1558bd1326ab12e5e1fd9ee987
mavideniz <dogancanmavideniz@gmail.com>
2021-09-16 12:30:00 +0300

    navigation link practise

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -9,17 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            GeometryReader{ geo in
-                Image("example")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: geo.size.width)
+        NavigationView{
+            List(0..<100){ row in
+                NavigationLink(destination: Text("Row \(row)")){
+                    Text("Hello Row \(row)")
+                }
             }
-                
+            .navigationBarTitle("SwiftUI")
         }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
